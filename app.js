afeae5b711ac79fdece6f3469f245e36fe31650a
Dany <>
2021-05-25 16:22:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var calc =  require('./calc');
+
+var x=50, y=20;
+console.log ('Addition = '+ calc.add(x,y));
+
+console.log('Subtraction = ' + calc.subtract(x,y));
+
+console.log('Multiplication = ' + calc.multiply(x,y));
+
+console.log('Division = ' + calc.div(x,y));
---
+++ calc.js
@@ -0,0 +1,17 @@
+// file name : calc.js
+
+exports.add = function(x, y){
+    return x+y;
+}
+
+exports.subtract = function(x,y){
+    return x-y;
+}
+
+exports.multiply = function(x,y){
+    return x*y;
+}
+
+exports.div = function(x,y){
+    return x/y;
+}
