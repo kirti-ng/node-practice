27372fded1d444a00be6ad2676f18c05974792c7
Dany <>
2021-05-25 16:42:25 +0530

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ console.log ('Addition = '+ calc.add(x,y));
 
 console.log('Subtraction = ' + calc.subtract(x,y));
 
-console.log('Multiplication = ' + calc.multiply(x,y));
+console.log('Multiplication1 = ' + calc.multiply(x,y));
 
 console.log('Division = ' + calc.div(x,y));
